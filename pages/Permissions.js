a91eb16f7bb5ce438ccad2a67a0f8df0a06568e6
Mikael Wistoft <mikaelwistoft@gmail.com>
2018-10-25 13:01:58 +0200

    Added microphone to permission page

--- pages/Permissions.js
+++ pages/Permissions.js
@@ -12,15 +12,19 @@ export default class Page extends React.Component {
 
 	state = {
 		hasCameraPermission: "Unknown",
-		hasRecordingPermission: "No API support",
+		hasRecordingPermission: "Unknown",
 		hasNotificationPermission: "No API support (android)",
 
 	}
-	
+
 	async componentWillMount() {
 
 		Permissions.check('camera').then(status => {
 			this.setState({ hasCameraPermission: status })
+		})
+
+		Permissions.check('microphone').then(status => {
+			this.setState({ hasRecordingPermission: status })
 		})
 
 		Permissions.check('notification').then(status => {
@@ -30,36 +34,43 @@ export default class Page extends React.Component {
 	}
 
 	requestPermissions = () => {
+		
 		Permissions.request('camera').then(status => {
 			this.setState({ hasCameraPermission: status })
-		  })
+		})
+		
+		Permissions.request('microphone').then(status => {
+			this.setState({ hasRecordingPermission: status })
+		})
+		
 		Permissions.request('notification').then(status => {
 			this.setState({ hasNotificationPermission: status })
-		  })
+		})
+
 	}
-	
+
 
 	render() {
 		return (
 
 			<Screen
-					title="Permissions"
-					onMenuPress={this.props.navigation.toggleDrawer}
-					style={{
-						alignItems: 'flex-start',
-					}}
-				>
+				title="Permissions"
+				onMenuPress={this.props.navigation.toggleDrawer}
+				style={{
+					alignItems: 'flex-start',
+				}}
+			>
 
 				<Text>Camera: {this.state.hasCameraPermission}</Text>
 				<Text>Recording: {this.state.hasRecordingPermission}</Text>
 				<Text>Notification: {this.state.hasNotificationPermission}</Text>
-			
+
 				<Button title="Request Permissions" onPress={this.requestPermissions.bind(this)} />
 
-				
-			</Screen>		
+
+			</Screen>
 		);
 	}
-	
+
 
 }
